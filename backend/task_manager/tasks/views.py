a580d3747c5434bece9f14327a162bf637f5335a
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2025-05-05 20:25:04 +0200

    Update views.py

--- backend/task_manager/tasks/views.py
+++ backend/task_manager/tasks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskListView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
